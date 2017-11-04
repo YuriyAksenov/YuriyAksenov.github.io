7a0b0da9390abf9a55b8c6b9bd79803ecc5a1a9b
Yuriy Aksenov <Yura290610@yandex.ru>
2017-11-05 00:33:18 +0300

    добавлены слайдеры и качества

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+    $(function(){
+  
+        $('.slider').slider();
+      $('.button-collapse').sideNav();
+      $('.parallax').parallax();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
